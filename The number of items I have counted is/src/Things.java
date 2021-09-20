f643ff478f4ac001ba2c70bfb3493b6168287a05
Gulfam <Gulfam@Gulfam-HP>
2021-09-20 02:14:52 -0500

    First Commit

---
+++ The number of items I have counted is/src/Things.java
@@ -0,0 +1,15 @@
+
+public class Things {
+
+	public static void main(String[] args) {
+		System.out.println("The Number Of Items I Have Counted Is");
+		
+		int myFirstNumber = 12;
+		int mySecondNumber = 10;
+		int myTotal = myFirstNumber + mySecondNumber;
+		System.out.println(myTotal);
+				
+
+	}
+
+}
