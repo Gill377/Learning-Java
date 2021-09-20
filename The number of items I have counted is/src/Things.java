8174c75db0a8d6270d1fc2b1ed6ce7c727f7cbec
Jeff Wilkins <jeff@cloudaccelerate.io>
2021-09-20 18:13:07 -0500

    Update with some comments.

--- The number of items I have counted is/src/Things.java
+++ The number of items I have counted is/src/Things.java
@@ -8,8 +8,17 @@ public class Things {
 		int mySecondNumber = 10;
 		int myTotal = myFirstNumber + mySecondNumber;
 		System.out.println(myTotal);
-				
-
+		
+		/*
+		 * This is correct.  This is an example of a comment.  Comments can be used to add clarity to your code.
+		 * It is a good practice to add them to complex parts of your code.  You can also comment out lines of
+		 * your code when working with it to remove them from execution for a time.
+		 */
+		//System.out.println("This should not run for a while.");
+		
+			//this is another example of a comment
+			//here is another way you could write the above command
+		System.out.println("The Number Of Items I Have Counted Is " + myTotal);		//also you can comment at the end
 	}
 
 }
